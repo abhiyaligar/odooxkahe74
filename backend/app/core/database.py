804136d426875d42bd4b335f330d9249a93cdd2d
Abhishek <abhijyaligar@gmail.com>
2026-06-20 13:51:03 +0530

    feat:Inital Auth (Signup And Login)

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -5,6 +5,7 @@ from app.core.config import settings
 SQLALCHEMY_DATABASE_URL = settings.DB_URL
 if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("+asyncpg", "")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
